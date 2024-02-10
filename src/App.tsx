5ccbe61b14e0da197d03a4e9c09397c198b7ac37
tirahman105 <tirahman105@gmail.com>
2024-02-10 16:45:28 +0600

    initial project set up

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <h1>To do app</h1>
+      <Button>Submit</Button>
+    </div>
+  );
+}
+
+export default App;
